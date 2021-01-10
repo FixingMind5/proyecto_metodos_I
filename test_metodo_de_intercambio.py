c7bfbeb7c775820ebf0f9f5da2874d948efae4a0
Manuel Aguilar <aguilarmanu5@gmail.com>
2021-01-10 06:40:25 -0600

    # Some bugs are fixed - Doolittle works at 100% (I hope xD) - Swap Gauss Jordan method idk wtf :'( but some logic were fixed

--- test_metodo_de_intercambio.py
+++ test_metodo_de_intercambio.py
@@ -4,7 +4,7 @@ from matrix import Matrix
 from vector import Vector
 
 
-def test_solve():
+def test_swap():
     """tests swap.solve()"""
     matrix = Matrix([
         [1, 2],
@@ -21,15 +21,43 @@ def test_solve():
     assert result_matrix == correct_matrix
 
 
-def test_solving():
-    """test swap.solve()"""
+def test_2x2_solving():
+    """tests swap.solve()"""
     matrix = Matrix([
-        [1, 2, 1],
-        [1, 1, 2],
-        [2, 1, 1]
+        [1, 2],
+        [2, 1]
     ])
-    vector = Vector([3, 2, 1])
+    vector = Vector([3, 2])
     swap = SwapMethod(matrix, vector)
     comprobation = swap.solve()
 
     assert comprobation == 3
+
+
+def test_3x3_solving():
+    """test swap.solve()"""
+    matrix = Matrix([
+        [3, 2, 1],
+        [5, 3, 4],
+        [1, 1, -1]
+    ])
+    vector = Vector([1, 2, 1])
+    swap = SwapMethod(matrix, vector)
+    comprobation = swap.solve()
+
+    assert comprobation == 1
+
+
+def test_4x4_solving():
+    """test swap.solve()"""
+    matrix = Matrix([
+        [3, 6, -2, 9],
+        [-5, 4, 5, -6],
+        [-3, 8, 2, -3],
+        [-4, 10, 3, 9]
+    ])
+    vector = Vector([6, 5, 3, 9])
+    swap = SwapMethod(matrix, vector)
+    comprobation = swap.solve()
+
+    assert comprobation == 6
